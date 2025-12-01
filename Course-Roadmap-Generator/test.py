2aa12388faffefa8ed39fb039fd6dfb45919627d
Naim Parvez <parveznaim0@gmail.com>
2025-12-01 22:20:59 +0600

    Update API key placeholder in test.py

--- Course-Roadmap-Generator/test.py
+++ Course-Roadmap-Generator/test.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAKem9QhQboP0mY0pee7T6TvlwtAI4mrWM")
+genai.configure(api_key="add your api key")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("generate a learning path of python for beginners that can be completed in 2 weeks")
 print(response.text)
